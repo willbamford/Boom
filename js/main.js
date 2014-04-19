6fbd872ae8d0f15ff5230adc8c1875f50cd1f313
Will Bamford <w.bamford@gmail.com>
2014-04-19 11:40:12 +0100

    Wiring up in main.

--- js/main.js
+++ js/main.js
@@ -1,47 +1,21 @@
 define(
     [
-        'grobo/logger'
+        'grobo/lib',
+        'grobo/canvas',
+        'grobo/input',
+        'grobo/state-manager',
+        'grobo/engine'
     ],
-    function (logger) {
-        logger.info('Hello World');
-    }
-);
+    function (lib, refCanvas, refInput, refStateManager, refEngine) {
+        var canvas = lib.create(refCanvas).init('canvas'),
+            input = lib.create(refInput).init(canvas),
+            stateManager = lib.create(refStateManager).init(input),
+            engine = lib.create(refEngine).init(stateManager);
 
-
-/*
-require.config({
-    urlArgs: 'bust=' + (new Date()).getTime()
-});
-*/
-/*
-require(
-    [
-        'lib',
-        'state-factory',
-        'state-manager',
-        'engine',
-        'canvas',
-        'input'
-    ],
-    function (lib, refStateFactory, refStateManager, refEngine, refCanvas, refInput) {
-
-        var canvas          = lib.create(refCanvas).init('canvas'),
-            input           = lib.create(refInput).init(canvas),
-            stateManager    = lib.create(refStateManager).init(input),
-            config = {
-                stateManager: stateManager,
-                canvas: canvas
-            },
-            stateFactory    = lib.create(refStateFactory).init(config),
-            engine          = lib.create(refEngine).init(stateManager);
-
-        stateManager.change(stateFactory.getGame());
         engine.start();
 
-        // window.setTimeout(function () { stateManager.push(menuState); }, 2000);
-        // window.setTimeout(function () { stateManager.pop(); }, 3000);
-        // window.setTimeout(function () { stateManager.push(menuState); }, 4000);
-        // window.setTimeout(function () { engine.stop(); }, 10000);
+        window.setTimeout(function () {
+            engine.stop();
+        }, 5000);
     }
 );
-*/
